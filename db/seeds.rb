beadd2878d973ed5bbc2307172b1b20fcca3389b
IgorSinitsa <shibary@bk.ru>
2024-08-15 17:03:35 +0300

    add seeds to task03

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Faker::Config.locale = :ru
+
+def range_rand
+  (0..rand(2..5))
+end
+
+range_rand.each do
+  User.create(user: Faker::Name.last_name, password: Faker::Alphanumeric.alpha(number: 10))
+end
+
+users_id = User.all.pluck(:id)
+
+range_rand.each do
+  category = Category.create(title: Faker::Lorem.word)
+
+  range_rand.each do
+    test = Test.create(title: Faker::Lorem.sentence(word_count: 3),
+                       level: rand(1..3),
+                       category_id: category.id,
+                       author_id: users_id[rand(5)])
+    range_rand.each do
+      question = Question.create(body: Faker::Lorem.question,
+                                 test_id: test.id)
+      range_rand.each do
+        Answer.create(body: Faker::Lorem.sentence(word_count: 3),
+                      correct: rand(0..1),
+                      question_id: question.id)
+      end
+    end
+  end
+end
