0cc638bb866d1072501ba5b7395f80bb625181ae
IgorSinitsa <shibary@bk.ru>
2024-08-17 16:30:51 +0300

    base task04

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,9 @@
 class Test < ApplicationRecord
+  belongs_to :user, :foreign_key => "author_id"
+  has_many :results
+  has_many :users, through: :results
+  has_many :questions
+
   def category(category)
     joins(:categories).
       where("categories.title = :category", category: category).
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  has_many :tests, :foreign_key => "author_id"
+  has_many :results
+  has_many :users, through: :results
+
   def self.test_level(level)
     Test.left_outer_joins(:results).
-    where("tests.level = :level", level: level).
-    where(results.user_id = self.id)
+      where("tests.level = :level", level: level).
+      where(results.user_id = self.id)
   end
 end
