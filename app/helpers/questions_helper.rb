dec63a38f1bb3d7d41dd092f79eaeb34606fb60b
IgorSinitsa <shibary@bk.ru>
2025-01-29 15:39:22 +0300

    fix valid answer

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -4,4 +4,8 @@ module QuestionsHelper
   def question_header(test)
     "#{TITLES[action_name.to_sym]} #{test.title} question"
   end
+
+  def check_valid_question(question)
+    question.answers_valid? ? "✓" : "✗"
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,11 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :gists, dependent: :destroy
   validates :body, presence: true
+
+  scope :valid, -> {joins(:answers).where("answers.correct = ?", true).group('questions.id')}
+
+  def answers_valid?
+    self.answers.where(correct: :true).size >=1
+  end
+
 end
