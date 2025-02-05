091f8bfacbd25b98371e6972deac8ab07edf5c4c
IgorSinitsa <shibary@bk.ru>
2025-02-05 20:49:49 +0300

    fix

--- app/models/result.rb
+++ app/models/result.rb
@@ -3,12 +3,17 @@ class Result < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: "Question", optional: true
   before_validation :before_validation_set_new_questions, on: :create
-  before_validation :set_current_question, on: [:create, :update]
+  before_validation :update_result, on: [:update]
+  # before_validation :set_current_question, on: [:create, :update]
 
   SUCCESS_SCORE = 85
 
   def test_passed?
     pass_test
+  end
+
+  def passed_test?
+    current_question.nil?
   end
 
   def index_question
@@ -25,15 +30,11 @@ class Result < ApplicationRecord
     self.correct_questions += 1 if correct_answer?(answer_ids)
   end
 
-  def passed_test?
-    current_question.nil?
-  end
+  private
 
   def set_poins
     self.point = self.total_questions > 0 ? ((self.correct_questions.to_f / self.total_questions.to_f) * 100) : 0
   end
-
-  private
 
   def array_questions
     test.questions.pluck(:id)
@@ -43,9 +44,17 @@ class Result < ApplicationRecord
     arr = test.array_questions_valid
     if arr.present?
       self.correct_questions = 0
-      self.questions = arr.sort_by{ rand }
+      self.questions = arr.sort_by { rand }
       self.total_questions = arr.count
     end
+  end
+
+  def update_result
+    if questions.empty?
+      set_poins
+      self.pass_test = point >= SUCCESS_SCORE ? true : false
+    end
+    set_current_question
   end
 
   def set_current_question
@@ -54,8 +63,6 @@ class Result < ApplicationRecord
       questions.shift
     else
       self.current_question = nil
-      set_poins
-      self.pass_test = point >= SUCCESS_SCORE ? true : false
     end
   end
 
